22d5c0158d516afd4098fc73d47a862aadab54c2
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2024-01-16 11:36:15 +0700

    buoi4

--- buoi_4/a2.sql
+++ buoi_4/a2.sql
@@ -1,5 +1,26 @@
-DECLARE @ten AS NVARCHAR(100)
+-- test
+CREATE OR ALTER PROCEDURE A2
+	@ten NVARCHAR(100),
+	@loichao NVARCHAR(100) OUTPUT
+AS
+	SET @loichao = N'Xin chào ' + @ten
+GO
 
-SET @ten = N'Nguyễn Văn A'
+DECLARE @ten_inp AS NVARCHAR(100), @loichao_out AS NVARCHAR(100)
+SET @ten_inp = N'Nguyễn Văn A'
+EXEC A2 @ten=@ten_inp, @loichao=@loichao_out OUTPUT
+PRINT @loichao_out
 
+
+
+GO
+CREATE OR ALTER PROCEDURE A2 @ten NVARCHAR(100)
+AS
 PRINT N'Xin chào ' + @ten
+GO 
+
+DECLARE @ten AS NVARCHAR(100)
+EXEC A2 @ten=N'Nguyễn Văn A'
+GO
+
+
